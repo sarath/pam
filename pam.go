ec08e7f48cde9d34031712310c92ab6d168a9cc3
Sarath Chandra Pandurangi <sarath@sarathonline.com>
2014-10-25 03:24:23 -0400

    extract()

--- pam.go
+++ pam.go
@@ -3,11 +3,13 @@ package main
 import (
 	"os"
 	"io/ioutil"
+//	"crypto/md5"
 	"encoding/json"
 	"path"
 	"log"
 	"net/http"
 	"io"
+	"os/exec"
 )
 
 type PamRcConf struct {
@@ -37,6 +39,7 @@ type PamConf struct {
 	Source struct {
 		Url  string
 		Type string
+		Checksum []byte
 	}
 	Path        []string
 	Env map[string]string
@@ -46,6 +49,8 @@ var (
 	pamrc *PamRcConf = new(PamRcConf)
 	PAM_EXTENSION    = ".pam.json"
 	PAM_DEF_RC       = "./config/pam.rc.json"
+	SEVENZA_LOC = "https://github.com/sarath/pam/blob/master/dist/7za.orig?raw=true"
+	SEVENZA_CHECKSUM = []byte("afdaf")
 )
 
 func main() {
@@ -66,7 +71,7 @@ func main() {
 
 	switch cmd {
 	case "install" :
-		install(pkg, len(os.Args) >3 && os.Args[3] == "-f")
+		install(pkg, len(os.Args) > 3 && os.Args[3] == "-f")
 	case "remove" :
 		remove(pkg)
 	default :
@@ -79,23 +84,60 @@ func main() {
 
 func install(pkg string, force bool) {
 	log.Printf("Installing: %v, ", pkg)
+
+	//read and cache config
 	var pamc PamConf
 	readPamConfig(pkg, &pamc)
 
 	//determine dest
-	dest := path.Join(pamrc.Cache, pamc.Name+"-"+pamc.Version+determineType(pamc))
-	log.Printf("version: %v, from: %s, to: %s", pamc.Version, pamc.Source.Url, dest)
-	err := downloadFile(pamc.Source.Url, dest, force)
+	archFileLoc := path.Join(pamrc.Cache, pamc.Name+"-"+pamc.Version+determineType(pamc))
+	log.Printf("version: %v, from: %s, to: %s", pamc.Version, pamc.Source.Url, archFileLoc)
+
+	//download
+	err := downloadFile(pamc.Source.Url, archFileLoc, force, pamc.Source.Checksum)
 	if err != nil {
-		log.Fatalf("Error while downloading %v", err)
+		log.Fatal("Error while downloading", err)
+	}
+
+	//extract
+	extractArchive(archFileLoc, path.Join(pamrc.Bin, pamc.Name))
+
+	//setup paths
+
+	//setup env
+
+
+}
+
+func extractArchive(file string, destination string) {
+	log.Println("Extracting:", file)
+	sevenza := detect7za()
+	commandSlice := []string{sevenza, "e", file, destination}
+	log.Println(commandSlice)
+	c := exec.Command(commandSlice[0], commandSlice[1:]...)
+	e := c.Run()
+	if e != nil {
+		log.Fatal("Error during extraction : ", e)
 	}
 }
 
-func downloadFile(src string, dest string, force bool) (err error) {
+func detect7za() string{
+	sevenza := path.Join(pamrc.Bin, "7za", "7za.exe")
+	if _, err := os.Stat(sevenza); os.IsNotExist(err) {
+		os.MkdirAll(path.Join(pamrc.Bin, "7za"), os.ModeDir)
+		err := downloadFile(SEVENZA_LOC, sevenza ,false, SEVENZA_CHECKSUM)
+		if err != nil {
+			log.Fatal("Error downloading 7za.exe, Must have 7za in ", sevenza , err)
+		}
+	}
+	return sevenza
+}
+
+func downloadFile(src string, dest string, force bool, checksum []byte) (err error) {
 
 	if _, err := os.Stat(dest); err == nil {
 		if (force) {
-			log.Println("%s exists, but force set, downloading",dest)
+			log.Println("%s exists, but force set, downloading", dest)
 		}else {
 			log.Printf("%s exists, skipping download", dest)
 			return nil
@@ -119,6 +161,11 @@ func downloadFile(src string, dest string, force bool) (err error) {
 		return
 	}
 	log.Printf("Download complete [%v bytes]", n)
+
+	if checksum != nil {
+
+//		todo: md5.Sum(out)
+	}
 	return
 }
 
